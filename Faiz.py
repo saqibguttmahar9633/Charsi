f160c2c255b7af12f00e274285036bc847898d7a
saqibguttmahar9633 <saqibguttmahar@gmail.com>
2025-09-23 22:11:14 +0500

    Update Faiz.py

--- Faiz.py
+++ Faiz.py
@@ -1,1 +1,11 @@
-
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[97;1m [•] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/DVq6v6lasB5LfIZb4D1NNl?mode=ems_copy_c')
+tokyo=platform.architecture()[0]
+if tokyo=="32bit":
+    os.system('clear')
+    print('\033[91;1m [•] 32 Bit Device Not Working')
+elif tokyo=="64bit":
+    __import__("mahar64")
